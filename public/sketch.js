a4d0ea7a819db467f72a05327b6eade80d350487
Erik Mekiassen <erik.mekiassen@spk.no>
2024-04-08 16:20:14 +0200

    fungrendes versjon. mangler settings i gui om det skal implementeres

--- public/sketch.js
+++ public/sketch.js
@@ -1,39 +1,96 @@
 
-let rad = 60; // Width of the shape
-let xpos, ypos; // Starting position of shape
-
-let xspeed = 2.8; // Speed of the shape
-let yspeed = 2.2; // Speed of the shape
-
-let xdirection = 1; // Left or Right
-let ydirection = 1; // Top to Bottom
-
+const canvasX = 720;
+const canvasY = 600;
+const offsettX = canvasX / 6;
+let initialTriangleObject = [{"x": offsettX, "y": canvasY / 1.2}];
 function setup() {
-  createCanvas(720, 400);
+  createCanvas(canvasX, canvasY);
   noStroke();
   frameRate(30);
   ellipseMode(RADIUS);
-  // Set the starting position of the shape
-  xpos = width / 2;
-  ypos = height / 2;
+  
+  background(102);
+  
+  stroke('purple');
+  strokeWeight(1);
+  const triangle = initialTrianglePoints(initialTriangleObject)
+  printLinesBetweenTrianglePoints(triangle)
+  TheChaosGame(triangle, 50000)
+  
+}
+function draw() {
 }
 
-function draw() {
-  background(102);
+function TheChaosGame(triangle, runs) {
+  let previouspoint = randomPointInsideTriangle(triangle);
 
-  // Update the position of the shape
-  xpos += xspeed * xdirection;
-  ypos = ypos + yspeed * ydirection;
+  stroke(255, 0, 0);
+  strokeWeight(10);
+  point(previouspoint.x, previouspoint.y);
+  stroke('purple');
+  strokeWeight(1);
 
-  // Test to see if the shape exceeds the boundaries of the screen
-  // If it does, reverse its direction by multiplying by -1
-  if (xpos > width - rad || xpos < rad) {
-    xdirection *= -1;
+  console.log("chaos", previouspoint.x, previouspoint.y);
+  for (let i = 0; i < runs; i++) {
+    previouspoint = calculateNextPoint(triangle, previouspoint)
+    console.log("bla", previouspoint.x, previouspoint.y);
+    point(previouspoint.x, previouspoint.y);
   }
-  if (ypos > height - rad || ypos < rad) {
-    ydirection *= -1;
+}
+
+function calculateNextPoint(triangle, vertex) {
+  let edgeIndex = Math.floor(Math.random() * triangle.length);
+  console.log("edgeIndex", edgeIndex);
+  let x = (triangle[edgeIndex].x + vertex.x) / 2;
+  let y = (triangle[edgeIndex].y + vertex.y) / 2;
+  console.log("calculateNextPoint", x, y);
+  return {"x": x, "y": y};
+}
+
+function randomPointInsideTriangle(triangle){
+  let x = Math.random().toFixed(3) * 1000;
+  let y = Math.random().toFixed(3) * 1000;
+
+  while(!checkIfPointIsInsideTriangle(triangle, x, y)) {
+    x = Math.random().toFixed(3) * 1000;
+    y = Math.random().toFixed(3) * 1000;
   }
 
-  // Draw the shape
-  ellipse(xpos, ypos, rad, rad);
+  return {"x": x, "y": y};
+}
+
+function checkIfPointIsInsideTriangle(triangle, x, y) {
+  if (
+    x > triangle[0].x && x < triangle[1].x &&
+    y < triangle[0].y && y > triangle[2].y
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function initialTrianglePoints(points) {
+  let startX = points[0].x
+  let startY = points[0].y
+  points.push({"x": canvasX / 1.2, "y": startY})
+  const middle = points[0].x / 2+ points[1].x / 2
+  console.log(points[0].x - offsettX);
+  console.log(middle);
+  points.push({"x": middle, "y": canvasY / 1.2 - middle})
+  
+  return points
+}
+
+function printLinesBetweenTrianglePoints(points) {
+  console.log(true);
+  
+  for (let i = 0; i < points.length; i++) {
+    if (points[i + 1] !== undefined) {
+      console.log(true);
+      line(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);
+    } else {
+      line(points[i].x, points[i].y, points[0].x, points[0].y);
+    }
+  }
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+if (process.env.NODE_ENV === "development") {
+  stopReportingRuntimeErrors(); // disables error overlays
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
